2f7806f826abe1c8cd4e78fd9b3e46efeb523a36
triflesandparsnips <118235879+triflesandparsnips@users.noreply.github.com>
2022-11-30 00:28:20 -0500

    Update Word.js

--- Word.js
+++ Word.js
@@ -19,7 +19,7 @@ class Word {
             textStyle(NORMAL);
         }
         fill(this.color);
-        text(this.word, this.x, this.y);
+        text(this.word, this.x, this.y, 100);
     }
 
     highlight() {
@@ -33,10 +33,10 @@ class Word {
     }
 
     intersect(x, y) {
-        let radius = 3;
+        let radius = 4;
 
-        let xIntersect = x > this.x - textWidth(this.word)/2 - radius && x < this.x + textWidth(this.word)/2 + radius;
-        let yIntersect = y > this.y - textSize(this.word)/2 - radius && y < this.y + textSize(this.word)/2 + radius;
+        let xIntersect = x > this.x - 100 - radius && x < this.x + 50 + radius;
+        let yIntersect = y > this.y - 20 - radius && y < this.y + 20 + radius;
 
         if (xIntersect && yIntersect) {
             return true;
